3b27255e027b3f70f2dd0d1f8e265ab4cf6f574a
helgaKalicz <kalicz.helga@gmail.com>
2017-08-15 15:43:14 +0200

    printing out dices

--- src/com/aka/games/Dice.java
+++ src/com/aka/games/Dice.java
@@ -1,5 +1,6 @@
 package com.aka.games;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Random;
@@ -22,20 +23,32 @@ public class Dice {
 
         attackerDice = diceList(attacker, attackerDice);
         defenderDice = diceList(defender, defenderDice);
+        System.out.println("Dice:");
+        System.out.print("  Attacker: ");
+        printResult(attackerDice);
+        System.out.print("  Defender: ");
+        printResult(defenderDice);
 
     }
 
     public ArrayList diceList (int units, ArrayList diceList ) {
         for (int i=0; i < units; i++) {
-
             int randomNr = random.nextInt(6) + 1;
             diceList.add(randomNr);
         }
         Collections.sort(diceList);
         Collections.reverse(diceList);
-
-        System.out.println(diceList);
         return diceList;
+    }
+    public void printResult(ArrayList diceList) {
+        for (int i = 0; i < diceList.size(); i++) {
+            if (i == diceList.size()-1) {
+                System.out.printf("%d\n", diceList.get(i));
+            }
+            else {
+                System.out.printf("%d-", diceList.get(i));
+            }
+        }
     }
 
 
