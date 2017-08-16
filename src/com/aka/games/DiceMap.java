f1af6175c99fec3b39aa151f3e5ce938d3e1fb5c
rtomil <rtomilobil@gmail.com>
2017-08-16 19:35:02 +0200

    add graphic update

--- src/com/aka/games/DiceMap.java
+++ src/com/aka/games/DiceMap.java
@@ -14,10 +14,28 @@ public class DiceMap {
 
     public String[][] initMatrix() {
         String[][] matrix= new String[20][144];
+        String player = "Player";
         for (int y = 0; y < 20; y++) {
             for (int x = 0; x < 144; x++) {
                 matrix[y][x] = " ";
             }
+        }
+        for (int playerCount = 3; playerCount < 15; playerCount++) {
+            for (int playerName = 0; playerName < player.length(); playerName++) {
+                matrix[playerCount][playerName] = Character.toString(player.charAt(playerName));
+            }
+            playerCount += 10;
+        }
+        matrix[3][7] = "1";
+        matrix[10][7] = "2";
+        matrix[10][0] = "F";
+        matrix[10][1] = "i";
+        matrix[10][2] = "e";
+        matrix[10][3] = "l";
+        matrix[10][4] = "d";
+        for (int i = 0; i < 144; i++) {
+            matrix[9][i] = "_";
+            matrix[11][i] = "‾";
         }
         return matrix;
     }
@@ -54,84 +72,84 @@ public class DiceMap {
             case "A":
                 // SNAIL
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 4;
+                startingCoordinates[1] = 14;
                 startingCoordinates[2] = unitNumber;
                 snail(startingCoordinates);
                 break;
             case "B":
                 // DOG
                 startingCoordinates[0] = 3+y;
-                startingCoordinates[1] = 16;
+                startingCoordinates[1] = 26;
                 startingCoordinates[2] = unitNumber;
                 dog(startingCoordinates);
                 break;
             case "C":
                 // FISH
                 startingCoordinates[0] = 2+y;
-                startingCoordinates[1] = 25;
+                startingCoordinates[1] = 35;
                 startingCoordinates[2] = unitNumber;
                 fish(startingCoordinates);
                 break;
             case "D":
                 // GHOSTSHARK
                 startingCoordinates[0] = 1+y;
-                startingCoordinates[1] = 35;
+                startingCoordinates[1] = 45;
                 startingCoordinates[2] = unitNumber;
                 ghostShark(startingCoordinates);
                 break;
             case "E":
                 // MAN
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 46;
+                startingCoordinates[1] = 56;
                 startingCoordinates[2] = unitNumber;
                 man(startingCoordinates);
                 break;
             case "F":
                 // WOMAN
                 startingCoordinates[0] = 2+y;
-                startingCoordinates[1] = 52;
+                startingCoordinates[1] = 62;
                 startingCoordinates[2] = unitNumber;
                 woman(startingCoordinates);
                 break;
             case "G":
                 // GIRAFFE
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 60;
+                startingCoordinates[1] = 70;
                 startingCoordinates[2] = unitNumber;
                 giraffe(startingCoordinates);
                 break;
             case "H":
                 // PIG
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 70;
+                startingCoordinates[1] = 80;
                 startingCoordinates[2] = unitNumber;
                 pig(startingCoordinates);
                 break;
             case "I":
                 // WIZARD
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 82;
+                startingCoordinates[1] = 92;
                 startingCoordinates[2] = unitNumber;
                 wizard(startingCoordinates);
                 break;
             case "J":
                 // CHICKEN
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 92;
+                startingCoordinates[1] = 102;
                 startingCoordinates[2] = unitNumber;
                 chicken(startingCoordinates);
                 break;
             case "K":
                 // RABBIT
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 100;
+                startingCoordinates[1] = 110;
                 startingCoordinates[2] = unitNumber;
                 rabbit(startingCoordinates);
                 break;
             case "L":
                 // SNAKE
                 startingCoordinates[0] = 5+y;
-                startingCoordinates[1] = 110;
+                startingCoordinates[1] = 120;
                 startingCoordinates[2] = unitNumber;
                 snake(startingCoordinates);
                 break;
@@ -159,6 +177,7 @@ public class DiceMap {
         matrix[y-2][x+8] = "@";
         matrix[y-2][x+9] = "@";
         matrix[y+2][x+4] = Integer.toString(health);
+        matrix[10][x+4] = "A";
     }
 
     private void chicken(int[] startingCoordinates){
@@ -175,6 +194,7 @@ public class DiceMap {
         matrix[y-3][x+4] = ">";
         matrix[y-4][x+2] = "_";
         matrix[y+2][x+2] = Integer.toString(health);
+        matrix[10][x+2] = "J";
     }
 
     private void wizard(int[] startingCoordinates){
@@ -194,6 +214,7 @@ public class DiceMap {
         matrix[y-3][x+1] = "/";
         matrix[y-3][x+2] = "\\";
         matrix[y+2][x+2] = Integer.toString(health);
+        matrix[10][x+2] = "I";
     }
 
     private void rabbit(int[] startingCoordinates){
@@ -218,6 +239,7 @@ public class DiceMap {
         matrix[y-2][x+3] = "(";
         matrix[y-2][x+4] = "\\";
         matrix[y+2][x+4] = Integer.toString(health);
+        matrix[10][x+4] = "K";
     }
 
     private void pig(int[] startingCoordinates){
@@ -247,6 +269,7 @@ public class DiceMap {
         matrix[y-3][x+7] = "_";
         matrix[y-3][x+8] = "_";
         matrix[y+2][x+4] = Integer.toString(health);
+        matrix[10][x+4] = "H";
 
     }
 
@@ -262,6 +285,7 @@ public class DiceMap {
         matrix[y-2][x+2] = "O";
         matrix[y-2][x+3] = "/";
         matrix[y+2][x+2] = Integer.toString(health);
+        matrix[10][x+2] = "E";
 
     }
 
@@ -277,6 +301,7 @@ public class DiceMap {
         matrix[y-2][x+2] = "O";
         matrix[y-2][x+3] = "/";
         matrix[y+2][x+2] = Integer.toString(health);
+        matrix[10][x+2] = "F";
 
     }
 
@@ -303,6 +328,7 @@ public class DiceMap {
         matrix[y-2][x+9] = "_";
         matrix[y-2][x+10] = "_";
         matrix[y+2][x+4] = Integer.toString(health);
+        matrix[10][x+4] = "L";
 
     }
 
@@ -330,6 +356,7 @@ public class DiceMap {
         matrix[y-5][x+5] = "_";
         matrix[y-5][x+6] = "_";
         matrix[y+2][x+2] = Integer.toString(health);
+        matrix[10][x+2] = "G";
     }
 
     private void dog(int[] startingCoordinates){
@@ -354,6 +381,7 @@ public class DiceMap {
         matrix[y][x+4] = "ʌ";
         matrix[y][x+5] = "ʌ";
         matrix[y+4][x+2] = Integer.toString(health);
+        matrix[10][x+2] = "B";
     }
 
     private void fish(int [] startingCoordinates){
@@ -379,6 +407,7 @@ public class DiceMap {
         matrix[y+1][x+6] = "∩";
         matrix[y+1][x+7] = "∩";
         matrix[y+5][x+4] = Integer.toString(health);
+        matrix[10][x+4] = "C";
     }
 
     private void ghostShark(int[] startingCoordinates){
@@ -406,5 +435,6 @@ public class DiceMap {
         matrix[y+4][x+3] = "/";
         matrix[y+4][x+4] = "\\";
         matrix[y+6][x+4] = Integer.toString(health);
+        matrix[10][x+4] = "D";
     }
 }
