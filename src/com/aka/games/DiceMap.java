325bc4e0513449105a7a06081d70cd443a837c9a
rtomil <rtomilobil@gmail.com>
2017-08-16 17:54:40 +0200

    change matrix y, add playerNr to startingpoint()

--- src/com/aka/games/DiceMap.java
+++ src/com/aka/games/DiceMap.java
@@ -1,9 +1,6 @@
 package com.aka.games;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class DiceMap {
 
@@ -17,7 +14,7 @@ public class DiceMap {
         this.p2Fields = p2Fields;
         this.p2Units = p2Units;
         this.matrix = initMatrix();
-
+        /*
         int[] startSnail = startingPoint("A", 9);
         snail(startSnail);
 
@@ -54,13 +51,13 @@ public class DiceMap {
         int[] startSnake = startingPoint("L", 9);
         snake(startSnake);
 
-        drawMatrix();
+        drawMatrix();*/
 
     }
 
     public String[][] initMatrix() {
-        String[][] matrix= new String[16][144];
-        for (int y = 0; y < 16; y++) {
+        String[][] matrix= new String[20][144];
+        for (int y = 0; y < 20; y++) {
             for (int x = 0; x < 144; x++) {
                 matrix[y][x] = " ";
             }
@@ -69,7 +66,7 @@ public class DiceMap {
     }
     
     public void drawMatrix(){
-        for (int x=0; x < 16; x++) {
+        for (int x=0; x < 20; x++) {
             for (int y=0; y < 144; y++) {
                 System.out.print(matrix[x][y]);
             }
@@ -78,70 +75,111 @@ public class DiceMap {
 
     }
 
-    private int[] startingPoint(String fieldName, int unitNumber) {
+    public void manipulateMatrix(HashMap<String, Integer> playerMap, int playerNr){
+        for (HashMap.Entry<String, Integer> entry: playerMap.entrySet()){
+            startingPoint(entry.getKey(), entry.getValue(), playerNr);
+            System.out.println(entry.getKey()+" "+ entry.getValue());
+        }
+    }
+
+    private int[] startingPoint(String fieldName, int unitNumber, int playerNr) {
         int[] startingCoordinates = new int[3];
+        int y = 0;
+        switch (playerNr){
+            case 1:
+                y = 0;
+                break;
+            case 2:
+                y = 11;
+                break;
+        }
         switch (fieldName){
             case "A":
                 // SNAIL
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 4;
+                startingCoordinates[2] = unitNumber;
+                snail(startingCoordinates);
                 break;
             case "B":
                 // DOG
-                startingCoordinates[0] = 3;
+                startingCoordinates[0] = 3+y;
                 startingCoordinates[1] = 16;
+                startingCoordinates[2] = unitNumber;
+                dog(startingCoordinates);
                 break;
             case "C":
                 // FISH
-                startingCoordinates[0] = 2;
+                startingCoordinates[0] = 2+y;
                 startingCoordinates[1] = 25;
+                startingCoordinates[2] = unitNumber;
+                fish(startingCoordinates);
                 break;
             case "D":
                 // GHOSTSHARK
-                startingCoordinates[0] = 1;
+                startingCoordinates[0] = 1+y;
                 startingCoordinates[1] = 35;
+                startingCoordinates[2] = unitNumber;
+                ghostShark(startingCoordinates);
                 break;
             case "E":
                 // MAN
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 46;
+                startingCoordinates[2] = unitNumber;
+                man(startingCoordinates);
                 break;
             case "F":
                 // WOMAN
-                startingCoordinates[0] = 2;
+                startingCoordinates[0] = 2+y;
                 startingCoordinates[1] = 52;
+                startingCoordinates[2] = unitNumber;
+                woman(startingCoordinates);
                 break;
             case "G":
                 // GIRAFFE
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 60;
+                startingCoordinates[2] = unitNumber;
+                giraffe(startingCoordinates);
                 break;
             case "H":
                 // PIG
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 70;
+                startingCoordinates[2] = unitNumber;
+                pig(startingCoordinates);
                 break;
             case "I":
                 // WIZARD
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 82;
+                startingCoordinates[2] = unitNumber;
+                wizard(startingCoordinates);
                 break;
             case "J":
                 // CHICKEN
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 92;
+                startingCoordinates[2] = unitNumber;
+                chicken(startingCoordinates);
                 break;
             case "K":
                 // RABBIT
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 100;
+                startingCoordinates[2] = unitNumber;
+                rabbit(startingCoordinates);
                 break;
             case "L":
                 // SNAKE
-                startingCoordinates[0] = 5;
+                startingCoordinates[0] = 5+y;
                 startingCoordinates[1] = 110;
+                startingCoordinates[2] = unitNumber;
+                snake(startingCoordinates);
+                break;
         }
-        startingCoordinates[2] = unitNumber;
+
         return startingCoordinates;
     }
 
