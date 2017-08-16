2b6aae92fc3bd8fb13698612cc495a570476c558
rtomil <rtomilobil@gmail.com>
2017-08-16 16:13:14 +0200

    create dog, fish, ghostshark

--- src/com/aka/games/DiceMap.java
+++ src/com/aka/games/DiceMap.java
@@ -17,44 +17,63 @@ public class DiceMap {
         this.p2Fields = p2Fields;
         this.p2Units = p2Units;
         this.matrix = initMatrix();
-        String a = "A";
-        int[] start = startingPoint(a);
-        snail(start);
+
+        int[] startSnail = startingPoint("A");
+        snail(startSnail);
+
+        int[] startDog = startingPoint("B");
+        dog(startDog);
+
+        int[] startFish = startingPoint("C");
+        fish(startFish);
+
+        int[] startGhostShark = startingPoint("D");
+        ghostShark(startGhostShark);
         drawMatrix();
 
     }
 
     public Character[][] initMatrix() {
-        Character[][] matrix = new Character[12][14];
-        for (int i=0; i < 12; i++) {
-            for (int j=0; j < 14; j++) {
-                matrix[i][j] = ' ';
+        Character[][] matrix= new Character[14][144];
+        for (int y = 0; y < 14; y++) {
+            for (int x = 0; x < 144; x++) {
+                matrix[y][x] = ' ';
             }
         }
         return matrix;
     }
     
     public void drawMatrix(){
-        for (int x=0; x < 12; x++) {
-            for (int y=0; y < 14; y++) {
+        for (int x=0; x < 14; x++) {
+            for (int y=0; y < 144; y++) {
                 System.out.print(matrix[x][y]);
             }
             System.out.println();
         }
+
     }
 
     private int[] startingPoint(String fieldName) {
         int[] startingCoordinates = new int[2];
         switch (fieldName){
             case "A":
-                startingCoordinates[0] = 1;
+                startingCoordinates[0] = 3;
                 startingCoordinates[1] = 4;
                 break;
             case "B":
+                // DOG
+                startingCoordinates[0] = 1;
+                startingCoordinates[1] = 16;
                 break;
             case "C":
+                // FISH
+                startingCoordinates[0] = 0;
+                startingCoordinates[1] = 25;
                 break;
             case "D":
+                // ghostShark
+                startingCoordinates[0] = 0;
+                startingCoordinates[1] = 35;
                 break;
             case "E":
                 break;
@@ -75,7 +94,7 @@ public class DiceMap {
     }
 
     private void snail(int[] startingCoordinates){
-        int y = startingCoordinates[0]+1;
+        int y = startingCoordinates[0];
         int x = startingCoordinates[1];
         matrix[y][x] = '_';
         matrix[y][x+1] = '_';
@@ -91,5 +110,75 @@ public class DiceMap {
         matrix[y-1][x+8] = '/';
         matrix[y-2][x+8] = '@';
         matrix[y-2][x+9] = '@';
+    }
+
+    private void dog(int[] startingCoordinates){
+        int y = startingCoordinates[0];
+        int x = startingCoordinates[1];
+        matrix[y+1][x] = '\\';
+        matrix[y+2][x] = '/';
+        matrix[y+2][x+1] = '\\';
+        matrix[y+2][x+2] = '/';
+        matrix[y+2][x+3] = '\\';
+        matrix[y+1][x+1] = '_';
+        matrix[y+1][x+2] = '_';
+        matrix[y+1][x+3] = '\\';
+        matrix[y+1][x+4] = '_';
+        matrix[y+1][x+5] = '_';
+        matrix[y+1][x+6] = '/';
+        matrix[y][x+6] = '\\';
+        matrix[y-1][x+5] = '_';
+        matrix[y-1][x+4] = '_';
+        matrix[y][x+3] = '/';
+        matrix[y][x+4] = 'ʌ';
+        matrix[y][x+5] = 'ʌ';
+    }
+
+    private void fish(int [] startingCoordinates){
+        int y = startingCoordinates[0];
+        int x = startingCoordinates[1];
+        matrix[y+1][x] = '\\';
+        matrix[y+1][x+1] = '\\';
+        matrix[y+2][x] = '/';
+        matrix[y+2][x+1] = '/';
+        matrix[y+1][x+2] = '_';
+        matrix[y+1][x+3] = '_';
+        matrix[y+1][x+4] = '/';
+        matrix[y+2][x+4] = '\\';
+        matrix[y][x+5] = '_';
+        matrix[y][x+6] = '_';
+        matrix[y][x+7] = '_';
+        matrix[y+2][x+5] = '_';
+        matrix[y+2][x+6] = '_';
+        matrix[y+2][x+7] = '_';
+        matrix[y+1][x+8] = '\\';
+        matrix[y+2][x+8] = '/';
+        matrix[y+1][x+6] = '∩';
+        matrix[y+1][x+7] = '∩';
+    }
+
+    private void ghostShark(int[] startingCoordinates){
+        int y = startingCoordinates[0];
+        int x = startingCoordinates[1];
+        matrix[y][x+3] = '_';
+        matrix[y][x+4] = '_';
+        matrix[y][x+5] = '_';
+        matrix[y][x+6] = '_';
+        matrix[y+1][x+2] = '/';
+        matrix[y+2][x+1] = '/';
+        matrix[y+2][x+2] = 'o';
+        matrix[y+2][x+5] = 'o';
+        matrix[y+3][x+1] = '|';
+        matrix[y+4][x+1] = '|';
+        matrix[y+1][x+7] = '\\';
+        matrix[y+2][x+8] = '\\';
+        matrix[y+3][x+8] = '|';
+        matrix[y+4][x+8] = '|';
+        matrix[y+3][x+3] = '/';
+        matrix[y+4][x+2] = '/';
+        matrix[y+4][x+5] = '\\';
+        matrix[y+3][x+4] = '\\';
+        matrix[y+4][x+3] = '/';
+        matrix[y+4][x+4] = '\\';
     }
 }
